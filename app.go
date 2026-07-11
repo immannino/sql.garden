eab905e63fc37748d3eb33e586380d020df05654
Tones <tonymannino@me.com>
2026-07-10 22:56:11 -0700

    Bump version to v0.0.1-alpha.2

--- app.go
+++ app.go
@@ -659,7 +659,7 @@ func (a *App) SaveS3Credentials(creds S3Credentials) error {
 	return a.persist.saveSetting(s3CredentialsKey, string(raw))
 }
 
-const appVersion = "v0.0.1-alpha.1"
+const appVersion = "v0.0.1-alpha.2"
 
 // GetAppVersion returns the current application version string.
 func (a *App) GetAppVersion() string { return appVersion }
