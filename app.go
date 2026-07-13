0481bf2955f29534f648803573fc82f31f00ba52
Tony Mannino <tonym@sb401k.com>
2026-07-13 12:05:31 -0700

    Bump version to v0.0.1-alpha.6

--- app.go
+++ app.go
@@ -666,7 +666,7 @@ func (a *App) SaveS3Credentials(creds S3Credentials) error {
 	return a.persist.saveSetting(s3CredentialsKey, string(raw))
 }
 
-const appVersion = "v0.0.1-alpha.5"
+const appVersion = "v0.0.1-alpha.6"
 
 // GetAppVersion returns the current application version string.
 func (a *App) GetAppVersion() string { return appVersion }
