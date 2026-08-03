968e7d050bd71edd07f9efd4d06d7acf93df4cbf
Tony Mannino <tonym@sb401k.com>
2026-08-03 09:55:46 -0700

    version fix

--- scripts/version_push.sh
+++ scripts/version_push.sh
@@ -5,5 +5,5 @@ sed -i '' "s/const appVersion = \".*\"/const appVersion = \"$VERSION\"/" app.go 
 git add wails.json app.go .github/workflows/release.yml && \
 git commit -m "Bump version to $VERSION" && \
 git tag $VERSION && \
-git push origin main && \
+# git push origin main && \
 git push origin $VERSION
